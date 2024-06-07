f8b0e32d2da2f2c6c965aae5e31569293ac2b0f3
Mateus Lora <loramateusatitus@gmail.com>
2024-06-07 10:54:06 -0300

    Conteúdo do game

---
+++ analise.py
@@ -0,0 +1,6 @@
+listaX  = [1,2,3]
+listaXAnalise  = [2,3,4]
+
+print( set(listaX).intersection(set(listaXAnalise)) )
+#print(len(set(listaX).intersection(set(listaXAnalise))  ))
+print(len(set(listaX)&(set(listaXAnalise))  ))
